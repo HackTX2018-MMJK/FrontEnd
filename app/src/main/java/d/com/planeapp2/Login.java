a7eaea5424ddcae15f0f10e09c812bdc128db21a
Matthew Bass <bassmatt0515@gmail.com>
2018-10-20 17:54:28 -0500

    added tabbed activity

--- app/src/main/java/d/com/planeapp2/Login.java
+++ app/src/main/java/d/com/planeapp2/Login.java
@@ -2,6 +2,11 @@ package d.com.planeapp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        EditText login_box = (EditText) findViewById(R.id.pinput);
+
+        login_box.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // API call gives error or row/seat combo]
+
+                    //ON SUCCESS
+
+                    setContentView(R.layout.activity_main_driver);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
